8c9992009da761370dba90ba33a4e64605fddf5b
Aliaksandr Shabor <as.habor@gmail.com>
2025-01-12 23:41:12 +0100

    some changes

--- src/features/setting-row.ui.tsx
+++ src/features/setting-row.ui.tsx
@@ -1,3 +1,4 @@
+import { ReactNode } from "react";
 import { Setting, SettingType } from "../entities/setting";
 import { Circles } from "./circles.ui";
 import { Squares } from "./squares.ui";
@@ -8,14 +9,20 @@ interface IProps {
 }
 
 export function SettingRow({ setting }: IProps) {
-	if (setting.type === SettingType.Circle) {
-		return <Circles />;
+	return Widgets.create(setting);
+}
+
+class Widgets {
+	static create(setting: Setting): ReactNode {
+		switch (setting.type) {
+			case SettingType.Circle:
+				return <Circles />;
+			case SettingType.Square:
+				return <Squares />;
+			case SettingType.Triangle:
+				return <Triangles />;
+			default:
+				throw new Error("Invalid setting type");
+		}
 	}
-	if (setting.type === SettingType.Square) {
-		return <Squares />;
-	}
-	if (setting.type === SettingType.Triangle) {
-		return <Triangles />;
-	}
-	throw new Error("Invalid setting type");
 }
